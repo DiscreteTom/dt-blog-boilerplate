148c2fde4346aaade4fce4be28ce62f7a6e18c55
DiscreteTom <discrete_tom@outlook.com>
2020-02-12 17:05:10 +0800

    optimize default layout app bar

--- dt-blog-boilderplate/layouts/default.vue
+++ dt-blog-boilderplate/layouts/default.vue
@@ -17,7 +17,7 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar clipped-left fixed app hide-on-scroll>
+    <v-app-bar clipped-left fixed app hide-on-scroll flat>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
     </v-app-bar>
