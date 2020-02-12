23b898862cf047b3f068806544381f6a9ac90cb1
DiscreteTom <discrete_tom@outlook.com>
2020-02-12 18:25:39 +0800

    update default layout, add app-bar action btns

--- dt-blog-boilderplate/layouts/default.vue
+++ dt-blog-boilderplate/layouts/default.vue
@@ -20,6 +20,45 @@
     <v-app-bar clipped-left fixed app hide-on-scroll flat>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
+
+      <v-spacer></v-spacer>
+
+      <!-- Home Btn -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on" @click="$router.push('/')">
+            <v-icon>mdi-home</v-icon>
+          </v-btn>
+        </template>
+        <span>Home</span>
+      </v-tooltip>
+      <!-- Github Btn -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on" :href="github">
+            <v-icon>mdi-github-circle</v-icon>
+          </v-btn>
+        </template>
+        <span>View on Github</span>
+      </v-tooltip>
+      <!-- Email Btn -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on" :href="'mailto:' + email">
+            <v-icon>mdi-email</v-icon>
+          </v-btn>
+        </template>
+        <span>Contact me</span>
+      </v-tooltip>
+      <!-- Share Btn -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on" :data-clipboard-text="url" id="shareBtn">
+            <v-icon>mdi-share-variant</v-icon>
+          </v-btn>
+        </template>
+        <span>Copy link</span>
+      </v-tooltip>
     </v-app-bar>
     <v-content>
       <v-container>
@@ -33,19 +72,37 @@
 </template>
 
 <script>
+import ClipboardJS from 'clipboard'
+
 export default {
   data() {
     return {
-      folders: process.env.contentFolders,
+      clipboard: null,
       drawer: false,
+      isMounted: false,
+      folders: process.env.contentFolders,
       title: process.env.config.title,
-      root: process.env.config.root
+      root: process.env.config.root,
+      github: process.env.config.github,
+      email: process.env.config.email
     }
   },
   filters: {
     capitalizeFirst(s) {
       return s.charAt(0).toUpperCase() + s.substring(1)
     }
+  },
+  computed: {
+    url() {
+      return this.isMounted ? window.location.href : ''
+    }
+  },
+  mounted() {
+    this.isMounted = true
+    this.clipboard = new ClipboardJS('#shareBtn')
+  },
+  beforeDestroy() {
+    this.clipboard.destroy()
   }
 }
 </script>
