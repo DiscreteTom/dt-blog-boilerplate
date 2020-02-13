60e337684dbc72f4846e0759ec952eff49d150ff
DiscreteTom <discrete_tom@outlook.com>
2020-02-13 17:20:41 +0800

    add breadcrumbs to pages/_.vue

--- dt-blog-boilderplate/pages/_.vue
+++ dt-blog-boilderplate/pages/_.vue
@@ -1,5 +1,12 @@
 <template>
-  <component :is="selectedArticle" />
+  <div>
+    <v-breadcrumbs :items="navs" large>
+      <template v-slot:divider>
+        <v-icon>mdi-chevron-right</v-icon>
+      </template>
+    </v-breadcrumbs>
+    <component :is="selectedArticle" />
+  </div>
 </template>
 
 <script>
@@ -10,13 +17,16 @@
 export default {
   data() {
     return {
+      navs: [],
       attributes: {},
       selectedArticle: null,
       content: process.env.content
     }
   },
   methods: {
+    // calculate `this.navs`, return rawPath
     getRawPath() {
+      this.navs = [{ text: '', href: '/' }]
       let paths = this.$route.params.pathMatch.split('/')
       let context = this.content
       let result = ''
@@ -25,13 +35,19 @@ export default {
         for (let j = 0; j < context.length; ++j) {
           if (paths[i] == context[j].name) {
             result += context[j].rawName
+            this.navs.push({
+              text: context[j].name,
+              href: paths.slice(0, i).join('/') + context[j].name
+            })
             context = context.children
             notFount = false
             break
           }
         }
-        if (notFount) return ''
+        if (notFount) return '' // TODO: goto 404
       }
+      // disable the last nav
+      this.navs[this.navs.length - 1].disabled = true
       return result
     }
   },
