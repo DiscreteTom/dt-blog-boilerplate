51587cf2f18c7ae482e09af8b0189096a6757c5e
DiscreteTom <discrete_tom@outlook.com>
2020-02-16 00:28:06 +0800

    fix BreadCrumbes show nav only if current page==`_.vue`

--- dt-blog-boilderplate/components/BreadCrumbs.vue
+++ dt-blog-boilderplate/components/BreadCrumbs.vue
@@ -1,6 +1,6 @@
 <template>
   <v-breadcrumbs
-    v-if="$store.state.navs.length"
+    v-if="$store.state.navs.length && $route.name == 'all'"
     :items="[{ text: '', href: '/' }].concat($store.state.navs)"
     large
   >
