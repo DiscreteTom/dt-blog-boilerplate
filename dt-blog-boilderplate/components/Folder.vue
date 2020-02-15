bb7c75aadc3413c2cac2266c99e2a5d8548a7e2c
DiscreteTom <discrete_tom@outlook.com>
2020-02-15 19:38:04 +0800

    fix folder children path

--- dt-blog-boilderplate/components/Folder.vue
+++ dt-blog-boilderplate/components/Folder.vue
@@ -5,7 +5,7 @@
       x-large
       class="justify-start"
       v-for="(dirent, i) in $store.state.context"
-      :to="$route.params.pathMatch + '/' + dirent.name"
+      :to="dirent.path"
       :key="i"
     >
       <v-icon class="mr-5">{{ dirent.icon }}</v-icon>
