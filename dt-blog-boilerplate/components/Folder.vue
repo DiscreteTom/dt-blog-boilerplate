e96d5f2fdc8a9f946e97a7c52b6cf6c44f66453b
DiscreteTom <discrete_tom@outlook.com>
2020-02-17 23:06:55 +0800

    optimize Folder.vue ui

--- dt-blog-boilerplate/components/Folder.vue
+++ dt-blog-boilerplate/components/Folder.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <v-item-group>
-      <v-container>
-        <v-item v-for="(dirent, i) in $store.state.current.children" :key="i">
-          <v-card hover :to="dirent.path">
-            <v-card-title>
-              <v-icon class="mr-5">{{ dirent.icon }}</v-icon>
-              {{ dirent.title }}
-              <v-icon v-if="dirent.isDir">mdi-chevron-right</v-icon>
-            </v-card-title>
-            <v-card-subtitle>
-              {{ dirent.description }}
-            </v-card-subtitle>
-          </v-card>
-        </v-item>
-      </v-container>
-    </v-item-group>
+    <v-hover
+      v-slot:default="{ hover }"
+      v-for="(dirent, i) in $store.state.current.children"
+      :key="i"
+    >
+      <v-card tile :to="dirent.path" :color="hover ? '#f5f5f5' : undefined">
+        <v-card-title>
+          <v-icon class="mr-5">{{ dirent.icon }}</v-icon>
+          {{ dirent.title }}
+          <v-icon v-if="dirent.isDir">mdi-chevron-right</v-icon>
+        </v-card-title>
+        <v-card-subtitle>
+          {{ dirent.description }}
+        </v-card-subtitle>
+      </v-card>
+    </v-hover>
   </div>
 </template>
