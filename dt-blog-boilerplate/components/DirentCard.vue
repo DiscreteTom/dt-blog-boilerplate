c80a8b23a0ba18b872937a282219c5601cb79c2b
DiscreteTom <discrete_tom@outlook.com>
2020-02-18 23:36:39 +0800

    optimize code, abstract to DirentCard.vue

---
+++ dt-blog-boilerplate/components/DirentCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-hover v-slot:default="{ hover }">
+    <v-card
+      tile
+      :to="dirent.path"
+      :style="{
+        'background-image': `url(${imgSrc})`,
+        'background-position': 'right',
+        'background-size': '70%'
+      }"
+    >
+      <!-- background image mask -->
+      <div
+        :style="
+          dirent.img
+            ? {
+                'background-image': 'linear-gradient(to right, #fff, #0000)',
+                'background-position': 'right',
+                'background-size': '70%'
+              }
+            : {}
+        "
+      >
+        <!-- hover shadow mask -->
+        <div
+          :style="{
+            'background-color': hover ? '#0000000d' : undefined
+          }"
+        >
+          <v-card-title>
+            <v-icon class="mr-5">{{ dirent.icon }}</v-icon>
+            {{ dirent.title }}
+            <v-icon v-if="dirent.isDir">mdi-chevron-right</v-icon>
+          </v-card-title>
+          <v-card-subtitle>
+            {{ dirent.description }}
+          </v-card-subtitle>
+        </div>
+      </div>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  props: {
+    dirent: Object
+  },
+  data() {
+    return {
+      imgSrc: ''
+    }
+  },
+  created() {
+    this.imgSrc = ''
+    if (this.dirent.img) {
+      import(
+        `~/../content/${this.dirent.rawPath
+          .split('/')
+          .slice(1, -1)
+          .join('/')}/_img/${this.dirent.img}`
+      ).then(data => {
+        this.imgSrc = data.default
+      })
+    }
+  }
+}
+</script>
--- dt-blog-boilerplate/components/Folder.vue
+++ dt-blog-boilerplate/components/Folder.vue
@@ -1,80 +1,18 @@
 <template>
   <div>
-    <v-hover
-      v-slot:default="{ hover }"
+    <DirentCard
       v-for="(dirent, i) in $store.state.current.children"
       :key="i"
+      :dirent="dirent"
     >
-      <v-card
-        tile
-        :to="dirent.path"
-        :style="{
-          'background-image': `url(${imgSrcs[i]})`,
-          'background-position': 'right',
-          'background-size': '70%'
-        }"
-      >
-        <!-- background image mask -->
-        <div
-          :style="
-            dirent.img
-              ? {
-                  'background-image': 'linear-gradient(to right, #fff, #0000)',
-                  'background-position': 'right',
-                  'background-size': '70%'
-                }
-              : {}
-          "
-        >
-          <div
-            :style="{
-              'background-color': hover ? '#0000000d' : undefined
-            }"
-          >
-            <v-card-title>
-              <v-icon class="mr-5">{{ dirent.icon }}</v-icon>
-              {{ dirent.title }}
-              <v-icon v-if="dirent.isDir">mdi-chevron-right</v-icon>
-            </v-card-title>
-            <v-card-subtitle>
-              {{ dirent.description }}
-            </v-card-subtitle>
-          </div>
-        </div>
-      </v-card>
-    </v-hover>
+    </DirentCard>
   </div>
 </template>
 
 <script>
+import DirentCard from './DirentCard'
+
 export default {
-  data() {
-    return {
-      imgSrcs: []
-    }
-  },
-  methods: {
-    refresh() {
-      this.imgSrcs = []
-      this.$store.state.current.children.map((dirent, i) => {
-        if (dirent.img) {
-          import(
-            `~/../content/${dirent.rawPath
-              .split('/')
-              .slice(1, -1)
-              .join('/')}/_img/${dirent.img}`
-          ).then(data => {
-            this.$set(this.imgSrcs, i, data.default)
-          })
-        }
-      })
-    }
-  },
-  watch: {
-    '$store.state.current.rawPath': 'refresh'
-  },
-  created() {
-    this.refresh()
-  }
+  components: { DirentCard }
 }
 </script>
