65ed3487ae4ae535e8e305dae23affc041ef20cd
DiscreteTom <discrete_tom@outlook.com>
2020-02-18 16:55:52 +0800

    update Folder, add img support

--- dt-blog-boilerplate/components/Folder.vue
+++ dt-blog-boilerplate/components/Folder.vue
@@ -6,6 +6,10 @@
       :key="i"
     >
       <v-card tile :to="dirent.path" :color="hover ? '#f5f5f5' : undefined">
+        <v-img
+          :src="imgSrcs[i]"
+        >
+        </v-img>
         <v-card-title>
           <v-icon class="mr-5">{{ dirent.icon }}</v-icon>
           {{ dirent.title }}
@@ -18,3 +22,36 @@
     </v-hover>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      imgSrcs: []
+    }
+  },
+  methods: {
+    refresh() {
+      this.imgSrcs = []
+      this.$store.state.current.children.map((dirent, i) => {
+        if (dirent.img) {
+          import(
+            `~/../content/${dirent.path
+              .split('/')
+              .slice(1, -1)
+              .join('/')}/_img/${dirent.img}`
+          ).then(data => {
+            this.$set(this.imgSrcs, i, data.default)
+          })
+        }
+      })
+    }
+  },
+  watch: {
+    '$store.state.current.rawPath': 'refresh'
+  },
+  created() {
+    this.refresh()
+  }
+}
+</script>
