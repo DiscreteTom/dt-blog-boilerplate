acaa2c8e936e9e9a6c26a82d0c4198ba6cab719e
DiscreteTom <discrete_tom@outlook.com>
2020-02-18 23:20:18 +0800

    update Folder, prettier dirent img

--- dt-blog-boilerplate/components/Folder.vue
+++ dt-blog-boilerplate/components/Folder.vue
@@ -5,19 +5,42 @@
       v-for="(dirent, i) in $store.state.current.children"
       :key="i"
     >
-      <v-card tile :to="dirent.path" :color="hover ? '#f5f5f5' : undefined">
-        <v-img
-          :src="imgSrcs[i]"
+      <v-card
+        tile
+        :to="dirent.path"
+        :style="{
+          'background-image': `url(${imgSrcs[i]})`,
+          'background-position': 'right',
+          'background-size': '70%'
+        }"
+      >
+        <!-- background image mask -->
+        <div
+          :style="
+            dirent.img
+              ? {
+                  'background-image': 'linear-gradient(to right, #fff, #0000)',
+                  'background-position': 'right',
+                  'background-size': '70%'
+                }
+              : {}
+          "
         >
-        </v-img>
-        <v-card-title>
-          <v-icon class="mr-5">{{ dirent.icon }}</v-icon>
-          {{ dirent.title }}
-          <v-icon v-if="dirent.isDir">mdi-chevron-right</v-icon>
-        </v-card-title>
-        <v-card-subtitle>
-          {{ dirent.description }}
-        </v-card-subtitle>
+          <div
+            :style="{
+              'background-color': hover ? '#0000000d' : undefined
+            }"
+          >
+            <v-card-title>
+              <v-icon class="mr-5">{{ dirent.icon }}</v-icon>
+              {{ dirent.title }}
+              <v-icon v-if="dirent.isDir">mdi-chevron-right</v-icon>
+            </v-card-title>
+            <v-card-subtitle>
+              {{ dirent.description }}
+            </v-card-subtitle>
+          </div>
+        </div>
       </v-card>
     </v-hover>
   </div>
