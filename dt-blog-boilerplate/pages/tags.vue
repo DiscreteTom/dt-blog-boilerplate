da4375df279e6d5a3b437e728042318dc8e83516
DiscreteTom <discrete_tom@outlook.com>
2020-02-19 11:34:43 +0800

    optimize tags page ui

--- dt-blog-boilerplate/pages/tags.vue
+++ dt-blog-boilerplate/pages/tags.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <v-expansion-panels>
+    <v-expansion-panels accordion tile hover>
       <v-expansion-panel
         v-for="(paths, tagName) in $store.state.tagMap"
         :key="tagName"
       >
-        <v-expansion-panel-header>{{ tagName }}</v-expansion-panel-header>
+        <v-expansion-panel-header>
+          <div>
+            <v-icon class="mr-5">mdi-tag</v-icon>
+            <span class="font-weight-bold">{{ tagName }}</span>
+            <span class="text--secondary">({{ paths.length }})</span>
+          </div>
+        </v-expansion-panel-header>
         <v-expansion-panel-content>
           <DirentCard
             v-for="path in paths"
