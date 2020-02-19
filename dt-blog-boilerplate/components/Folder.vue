d48bc8a4d22c0ec9318bb563f6965535671be595
DiscreteTom <discrete_tom@outlook.com>
2020-02-19 16:43:18 +0800

    optimize Folder ui

--- dt-blog-boilerplate/components/Folder.vue
+++ dt-blog-boilerplate/components/Folder.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <!-- header -->
+    <div class="ml-5">
+      <h1>
+        {{ $store.state.current.title }}
+        <v-icon>mdi-chevron-right</v-icon>
+      </h1>
+      <p class="text--secondary">{{ $store.state.current.description }}</p>
+    </div>
     <DirentCard
       v-for="dirent in $store.state.current.children"
       :key="dirent.path"
