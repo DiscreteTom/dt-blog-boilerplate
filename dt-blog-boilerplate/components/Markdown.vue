ffd13ca1038b075d5e5140c6dc40008ca81694ea
DiscreteTom <discrete_tom@outlook.com>
2020-02-22 17:54:35 +0800

    abstract TOC as a component

--- dt-blog-boilerplate/components/Markdown.vue
+++ dt-blog-boilerplate/components/Markdown.vue
@@ -10,21 +10,7 @@
     <v-row class="flex-row-reverse">
       <!-- TOC -->
       <v-col cols="3">
-        <v-list dense v-if="$store.state.current.toc">
-          <v-subheader>TOC</v-subheader>
-          <v-list-item
-            v-for="(t, i) in $store.state.current.children"
-            :key="i"
-            @click="$vuetify.goTo('#' + t.slug)"
-          >
-            <v-list-item-content>
-              <v-list-item-title>
-                <span v-for="x in t.lvl - 1" :key="x"> - </span>
-                {{ t.content }}
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
+        <TOC></TOC>
       </v-col>
       <!-- Content -->
       <v-col>
@@ -37,7 +23,9 @@
 </template>
 
 <script>
+import TOC from './TOC'
 export default {
+  components: { TOC },
   data() {
     return {
       attributes: {},
---
+++ dt-blog-boilerplate/components/TOC.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-list dense v-if="$store.state.current.toc">
+    <v-subheader>TOC</v-subheader>
+    <v-list-item
+      v-for="(t, i) in $store.state.current.children"
+      :key="i"
+      @click="$vuetify.goTo('#' + t.slug)"
+    >
+      <v-list-item-content>
+        <v-list-item-title>
+          <span v-for="x in t.lvl - 1" :key="x"> - </span>
+          {{ t.content }}
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
