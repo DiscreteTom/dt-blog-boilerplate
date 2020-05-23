9d5d1523bd426407b78dd9558ed76a41396f9e4a
DiscreteTom <discrete_tom@outlook.com>
2020-05-23 22:12:16 +0800

    fix middleware/content

--- dt-blog-boilerplate/middleware/content.js
+++ dt-blog-boilerplate/middleware/content.js
@@ -1,5 +1,9 @@
 export default function({ route, redirect, store }) {
-  let dirent = store.state.pathMap[route.path]
+  let path = route.path
+  if (path.endsWith('/')) {
+    path = path.slice(0, -1)
+  }
+  let dirent = store.state.pathMap[path]
   if (dirent == null) {
     redirect('/404')
     return
