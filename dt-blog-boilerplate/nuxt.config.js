d34cf13ba88111033f9c3effa17116eb1c4fc200
DiscreteTom <discrete_tom@outlook.com>
2021-07-08 19:26:49 +0800

    use scrollBehaviour file instead of in nuxt.config

--- dt-blog-boilerplate/nuxt.config.js
+++ dt-blog-boilerplate/nuxt.config.js
@@ -76,22 +76,7 @@ export default {
     treeShake: true
   },
   router: {
-    middleware: 'global',
-    scrollBehavior(to, from, savedPosition) {
-      // if in same page
-      if (to.path == from.path) {
-        if (to.hash) {
-          // same page click anchor
-          return {} // don't scroll, use $vuetify.goTo to get a correct position
-        } else {
-          // no anchor, go to top
-          return { x: 0, y: 0 }
-        }
-      } else {
-        // different page, go to top
-        return { x: 0, y: 0 }
-      }
-    }
+    middleware: 'global'
   },
   ssr: false,
   build: {
